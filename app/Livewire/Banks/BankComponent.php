9ba7726bc6fa2cba752988a8317765136b809adf
Shazabch <shahzaib.ch2019@gmail.com>
2025-03-28 14:39:17 +0500

    Revert "."
    This reverts commit 77b1881cb939eec4a99bcb7056bb44645a120e25.

--- app/Livewire/Banks/BankComponent.php
+++ app/Livewire/Banks/BankComponent.php
@@ -2,62 +2,18 @@
 
 namespace App\Livewire\Banks;
 
-use App\Models\Bank;
 use Livewire\Component;
-use Livewire\WithPagination;
 
 class BankComponent extends Component
 {
-    use WithPagination;
-    public $bank;
-    public $search = '';
-    public $perPage = 10;
-    public $name;
-
-    protected $rules = [
-        'bank.name' => 'required|string|max:255',
-    ];
+    public $pageTitle;
 
     public function mount()
     {
-        $this->bank = new Bank(); // Initialize the model for units
-    }
-    public function addNew()
-    {
-        $this->bank = new Bank(); // Reset the bank model
-        $this->resetInputFields();
-    }
-    public function editItem($id)
-    {
-        $this->bank = Bank::find($id);
-    }
-    public function deleteItem($id)
-    {
-        Bank::find($id)->delete();
-        $this->dispatchBrowserEvent('success-notification', [
-            'message' => 'Bank deleted successfully.'
-        ]);
-    }
-    public function addEntry()
-    {
-        $this->bank = $this->bank ?? new Bank(); // Ensure bank is an instance
-        $this->validate();
-        $this->bank->save();
-        $this->resetInputFields();
-        $this->dispatchBrowserEvent('close-modal', ['id' => 'bankComponentModal']);
-        $this->dispatchBrowserEvent('success-notification', [
-            'message' => 'Bank added successfully.'
-        ]);
-    }
-    private function resetInputFields()
-    {
-        $this->name = ''; // Reset unit name
-
+        $this->pageTitle = 'All Banks';
     }
     public function render()
     {
-        $banks = Bank::where('name', 'like', '%' . $this->search . '%')
-            ->paginate($this->perPage);
-        return view('livewire.banks.bank-component', compact('banks'));
+        return view('livewire.banks.bank-component');
     }
 }
